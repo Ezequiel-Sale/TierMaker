1abbdb7e777ebf06613c411bcb14b50ed8c63559
Ezequiel Sale <ezequielsale123@gmail.com>
2024-08-18 12:03:54 -0300

    se creo la previsualización de las imagenes al pasar por encima de la tier, la carga de multiples archivos y ahora se puede descargar una imagen con la tier terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import html2canvas from 'html2canvas-pro';
 
 function App() {
   const tierList = ["S", "A", "B", "C", "D", "E"];
@@ -8,7 +9,8 @@ function App() {
   useEffect(() => {
     const imageInput = document.querySelector("#image-input");
     const itemsSection = document.querySelector("#selector-items");
-
+    const resetButton = document.querySelector("#reset-tier-button");
+    const saveButton = document.querySelector("#save-tier-button");
     const createItem = (src) => {
       const img = document.createElement("img");
       img.draggable = true;
@@ -20,16 +22,21 @@ function App() {
       return img;
     };
 
-    imageInput.addEventListener("change", (e) => {
-      const [file] = e.target.files;
-
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          createItem(e.target.result);
-        };
-        reader.readAsDataURL(file);
+    function filesToCreateItems(files) {
+      if (files && files.length > 0) {
+        Array.from(files).forEach((file) => {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            createItem(e.target.result);
+          };
+          reader.readAsDataURL(file);
+        });
       }
+    }
+
+    imageInput.addEventListener("change", (e) => {
+      const { files } = e.target;
+      filesToCreateItems(files);
     });
 
     let draggedElement = null;
@@ -50,18 +57,29 @@ function App() {
         currentTarget.appendChild(img);
       }
       currentTarget.classList.remove("drag-over");
+      currentTarget.querySelector(".drag-preview")?.remove();
     };
+
     const handleDragOver = (e) => {
       e.preventDefault();
       const { currentTarget } = e;
       if (sourceContainer === currentTarget) return;
 
       currentTarget.classList.add("drag-over");
+
+      const dragPreview = currentTarget.querySelector(".drag-preview");
+      if (draggedElement && !dragPreview) {
+        const previewElement = draggedElement.cloneNode(true);
+        previewElement.classList.add("drag-preview");
+        currentTarget.appendChild(previewElement);
+      }
     };
     const handleDragLeave = (e) => {
       e.preventDefault();
       const { currentTarget } = e;
       currentTarget.classList.remove("drag-over");
+      currentTarget.classList.remove("drag-files");
+      currentTarget.querySelector(".drag-preview")?.remove();
     };
 
     rows.forEach((row) => {
@@ -74,6 +92,52 @@ function App() {
     itemsSection.addEventListener("dragover", handleDragOver);
     itemsSection.addEventListener("dragleave", handleDragLeave);
 
+    itemsSection.addEventListener("drop", handleDropFromDesktop);
+    itemsSection.addEventListener("dragover", handleDragOverFromDesktop);
+
+    function handleDragOverFromDesktop(e) {
+      e.preventDefault();
+      const { currentTarget, dataTransfer } = e;
+      if (dataTransfer.types.includes("Files")) {
+        currentTarget.classList.add("drag-files");
+      }
+    }
+    // function handleDropFromDesktop(e) {
+    //   e.preventDefault();
+    //   const { currentTarget, dataTransfer } = e;
+
+    //   if (dataTransfer.types.includes("Files")) {
+    //     currentTarget.classList.remove("drag-files");
+    //     const { files } = dataTransfer;
+    //     filesToCreateItems(files);
+    //   }
+    // }
+    function handleDropFromDesktop(e) {
+      e.preventDefault();
+      const { currentTarget, dataTransfer } = e;
+      
+      if (dataTransfer.types.includes("Files")) {
+        currentTarget.classList.remove("drag-files");
+        const { files } = dataTransfer;
+    
+        Array.from(files).forEach((file) => {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            const existingImage = Array.from(itemsSection.querySelectorAll("img")).find(
+              (img) => img.src === e.target.result
+            );
+    
+            // Si no existe una imagen con el mismo src, la creamos y agregamos
+            if (!existingImage) {
+              createItem(e.target.result);
+            }
+          };
+          reader.readAsDataURL(file);
+        });
+      }
+    }
+    
+
     const handleDragStart = (e) => {
       draggedElement = e.target;
       sourceContainer = draggedElement.parentNode;
@@ -83,6 +147,41 @@ function App() {
     const handleDragEnd = () => {
       draggedElement = null;
       sourceContainer = null;
+    };
+
+    if (resetButton) {
+      resetButton.addEventListener("click", () => {
+        const items = document.querySelectorAll(".tier .item-image");
+        items.forEach((item) => {
+          item.remove();
+          itemsSection.appendChild(item);
+        });
+      });
+    }
+
+    saveButton.addEventListener("click", () => {
+      const tierContainer = document.querySelector(".tier");
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      html2canvas(tierContainer).then((canvas) => {
+        ctx.drawImage(canvas, 0, 0);
+        const imgURL = canvas.toDataURL("image/png");
+        const link = document.createElement("a");
+        link.download = "tier.png";
+        link.href = imgURL;
+        link.click();
+      })
+    })
+
+
+    return () => {
+      itemsSection.removeEventListener("dragover", handleDragOverFromDesktop);
+      itemsSection.removeEventListener("drop", handleDropFromDesktop);
+      rows.forEach((row) => {
+        row.removeEventListener("drop", handleDrop);
+        row.removeEventListener("dragover", handleDragOver);
+        row.removeEventListener("dragleave", handleDragLeave);
+      });
     };
   }, []);
 
@@ -123,9 +222,15 @@ function App() {
               <path d="M9 12h6" />
               <path d="M12 9v6" />
             </svg>
-            <input accept="image/*" type="file" id="image-input" hidden />
+            <input
+              multiple
+              accept="image/*"
+              type="file"
+              id="image-input"
+              hidden
+            />
           </label>
-          <label>
+          <label id="reset-tier-button">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -141,55 +246,7 @@ function App() {
               <path d="M4.05 11a8 8 0 1 1 .5 4m-.5 5v-5h5" />
             </svg>
           </label>
-        </section>
-        <div id="selector-items"></div>
-      </footer>
-    </>
-  );
-}
-
-export default App;
-
-{
-  /* <header className="top-header">
-        <img src="https://tiermaker.com/images/tiermaker-logo.png" alt="" />
-      </header>
-
-      <section className="tier">
-        <div className="row">
-          <aside className="label" style={{ "--level": "var(--color-s)" }}>
-            <span contentEditable="true">S</span>
-          </aside>
-        </div>
-        <div className="row">
-          <aside className="label" style={{ "--level": "var(--color-a)" }}>
-            <span contentEditable="true">A</span>
-          </aside>
-        </div>
-        <div className="row">
-          <aside className="label" style={{ "--level": "var(--color-b)" }}>
-            <span contentEditable="true">B</span>
-          </aside>
-        </div>
-        <div className="row">
-          <aside className="label" style={{ "--level": "var(--color-c)" }}>
-            <span contentEditable="true">C</span>
-          </aside>
-        </div>
-        <div className="row">
-          <aside className="label" style={{ "--level": "var(--color-d)" }}>
-            <span contentEditable="true">D</span>
-          </aside>
-        </div>
-        <div className="row">
-          <aside className="label" style={{ "--level": "var(--color-e)" }}>
-            <span contentEditable="true">E</span>
-          </aside>
-        </div>
-      </section>
-      <footer id="selector">
-        <section id="selector-buttons">
-          <label>
+          <label id="save-tier-button">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -200,29 +257,19 @@ export default App;
               strokeWidth="1"
               strokeLinecap="round"
               strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-device-floppy"
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
-              <path d="M9 12h6" />
-              <path d="M12 9v6" />
+              <path d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2" />
+              <path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+              <path d="M14 4l0 4l-6 0l0 -4" />
             </svg>
-            <input type="file" id="image-input" hidden/>
           </label>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="1"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M4.05 11a8 8 0 1 1 .5 4m-.5 5v-5h5" />
-          </svg>
         </section>
-      </footer> */
+        <div id="selector-items"></div>
+      </footer>
+    </>
+  );
 }
+
+export default App;
